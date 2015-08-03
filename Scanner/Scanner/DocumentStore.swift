ecd387b561a2e21910b57207e450a3bece007e0c
Tamás Zahola <tzahola@gmail.com>
2015-08-03 18:23:37 +0200

    DocumentStore progress

--- Scanner/Scanner/DocumentStore.swift
+++ Scanner/Scanner/DocumentStore.swift
@@ -8,20 +8,58 @@
 
 import Foundation
 
-class Page {
-
+class Page : Equatable {
 }
 
-class Document {
-    private(set) var pages:[Page]?
+func ==(a:Page, b:Page) -> Bool {
+    return a === b
+}
+
+class Document : Equatable {
+    let store:DocumentStore
+    
+    private(set) var pages:[Page] = []
+    
+    private init(_ store:DocumentStore) {
+        self.store = store
+    }
+    
+    func createPage() -> Page {
+        let page = Page()
+        self.pages.append(page)
+        return page
+    }
+}
+
+func ==(a:Document, b:Document) -> Bool {
+    return a === b
 }
 
 class DocumentStore {
-    
     private let fileManager:NSFileManager
     
-    init?(_ fileManager:NSFileManager) {
+    let path:String
+    private(set) var documents:[Document]
+    
+    init?(_ fileManager:NSFileManager, _ path:String) {
         self.fileManager = fileManager
+        self.path = path
+        self.documents = []
+        
+        if !self.fileManager.fileExistsAtPath(path) {
+            var error:NSErrorPointer = nil
+            let didCreate = self.fileManager.createDirectoryAtPath(path, withIntermediateDirectories: true, attributes: nil, error: error)
+            
+            if !didCreate {
+                return nil
+            }
+        }
+    }
+    
+    func save() -> Void {
+        let infoFilePath = self.path.stringByAppendingPathComponent("info")
+        let documentsData = NSKeyedArchiver.archivedDataWithRootObject(documents)
+        self.fileManager.createFileAtPath(infoFilePath, contents: documentsData, attributes: nil)
     }
     
 }
